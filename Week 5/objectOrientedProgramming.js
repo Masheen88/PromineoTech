e6e4908e305efd141f44ab3ba7b3cd6e4a85c257
Masheen1988 <matthew.cox@sumeffect.com>
2022-02-20 19:23:01 -0600

    added additional js files

---
+++ Week 5/objectOrientedProgramming.js
@@ -0,0 +1,39 @@
+/*Object Oriented Programming - Individual objects that can interact with other objects. Allows a 
+much more maintainable code.
+1. Abstraction: Allows you to hide complexity and instead for an overview of an object.
+ie. Vehicles all move, may move differently, but they all move.
+2. Encapsulation: Hiding the details of 'How' a method works.
+ie. When driving, what does the engine do? YOu do not have to know, it is hidden.
+3. Inheritance: Allows objects to inherit properties / functions from other properties.
+4. Polymorphism: Allows objects to act differently depending on how they are called.
+ie. Each animal communicates differently. All animals communicate but depending on the animal and how it communicates.
+
+Objects can be created from classes.
+Classes are like blueprint for objects to be created from.
+An object is an instance of a class.
+Objects and Classes are used interchangeably.
+
+
+The attributes and functions make up the description of our example DVD player
+
+DVD Player
+
+----
+
+Height
+Weight
+Width
+Color
+
+Play
+Fast Forward
+Rewind
+Pause
+
+DVD
+----
+
+Movie Length
+Image
+Size
+*/
