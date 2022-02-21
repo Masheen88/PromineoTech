c2eded9a5871a43232afed6c92613fdada4e781e
Masheen1988 <matthew.cox@sumeffect.com>
2022-02-20 18:55:46 -0600

    added week 5

--- Week 4/promises.js
+++ Week 4/promises.js
@@ -1,6 +1,6 @@
 //promises solves the same as problem as a callback but the code is much cleaner.
-//It is an object that represents the eventual completion or failure of an asynchronously operation.
-//A synchronous fuunctions can return a promise.
+//It is an object that represents the eventual completion or failure of an asynchronous operation.
+//Asynchronous functions can return a promise.
 //ie return something if successful or if failed return something else.
 
 //Uses Then and Catch methods allow to handle these processes.
