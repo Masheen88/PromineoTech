af59191982da4c70883ec9949fa5eb8f68b8eaac
Masheen1988 <matthew.cox@sumeffect.com>
2022-03-01 10:09:19 -0600

    shuffle and deal cards

--- Week_6/warGame.js
+++ Week_6/warGame.js
@@ -64,6 +64,12 @@ let startPlayers = new Players();
 let gamePlayers = startPlayers.createPlayers();
 console.log("Players:", gamePlayers);
 
+let Player1 = gamePlayers[0];
+let Player2 = gamePlayers[1];
+
+console.log("Player 1 is:", Player1);
+console.log("Player 2 is:", Player2);
+
 //Declares the deck of cards to be used in the game - Begin
 alert("Click Ok to get a deck of cards.");
 let newDeckOfCards = new deckOfCards();
@@ -71,16 +77,38 @@ let gameDeckOfCards = newDeckOfCards.cardDeck;
 console.log("Deck of Cards:", gameDeckOfCards);
 
 //Shuffles the deck of cards
-function shuffleDeckOfCards(deckOfCards) {
+function shuffleDeckOfCards(gameDeckOfCards) {
   for (
-    let indexOfCards = deckOfCards.length - 1;
+    let indexOfCards = gameDeckOfCards.length - 1;
     indexOfCards > 0;
     indexOfCards--
   ) {
     let indexOfShuffledCards = Math.floor(Math.random() * (indexOfCards + 1));
-    let shuffledCardDeck = deckOfCards[indexOfCards];
-    deckOfCards[indexOfCards] = deckOfCards[indexOfShuffledCards];
-    deckOfCards[indexOfShuffledCards] = shuffledCardDeck;
+    let shuffledGameDeckOfCards = gameDeckOfCards[indexOfCards];
+    gameDeckOfCards[indexOfCards] = gameDeckOfCards[indexOfShuffledCards];
+    gameDeckOfCards[indexOfShuffledCards] = shuffledGameDeckOfCards;
   }
-  return shuffleCardDeck;
+  return gameDeckOfCards;
 }
+
+//Declares the shuffled game deck of cards - Begin
+alert("Click Ok to get a shuffle your deck of cards.");
+let shuffledGameDeckOfCards = shuffleDeckOfCards(gameDeckOfCards);
+console.log("Deck of ShuffledCards:", shuffledGameDeckOfCards);
+
+//Equally deals the cards to each player.
+function dealCards(shuffledGameDeckOfCards) {
+  let half = Math.ceil(shuffledGameDeckOfCards.length / 2);
+  let player1Hand = shuffledGameDeckOfCards.slice(0, half);
+  let player2Hand = shuffledGameDeckOfCards.slice(-half);
+
+  return [player1Hand, player2Hand];
+}
+
+let playerHands = dealCards(shuffledGameDeckOfCards);
+console.log("Player Hands:", playerHands);
+let player1Hands = playerHands[0];
+let player2Hands = playerHands[1];
+
+console.log("Player 1 hand is:", player1Hands);
+console.log("Player 2 hand is:", player2Hands);
