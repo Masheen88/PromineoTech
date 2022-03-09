3688b75d2ab1b9d2c5957f6b713bd446a5c68cc3
Matthew Cox <mattdrummerguy@gmail.com>
2022-03-09 13:28:34 -0600

    mocha/chai test complete

--- Week_6/index_test.js
+++ Week_6/index_test.js
@@ -2,17 +2,24 @@ var expect = chai.expect;
 
 describe("Is the Player's name a string?", function () {
   describe("#createPlayers", function () {
-    it("should see if the characters in the prompt submitted are a string.", function () {
+    it("should see if players returned in the prompt are a string.", (done) => {
+      var string = ["Matthew", "Test"];
       var newPlayers = new Players();
-      var gamePlayers = newPlayers.createPlayers();
-      console.log("Test", gamePlayers);
-      expect().to.equal("Matthew", "Test");
-    });
+      let gamePlayers = newPlayers.createPlayers();
+      // console.log("Test", gamePlayers);
+      expect(gamePlayers).to.eql(string);
+      done();
+    })
+      .timeout(10000)
+      .catch(done);
 
-    it("should throw an error if the Player name is not a string.", function () {
+    it("should throw an error if the Player name is not a string.", (done) => {
       expect(function () {
         newPlayers.createPlayers("Matthew123", "Test123");
       }).to.throw(Error);
-    });
+      done();
+    })
+      .timeout(10000)
+      .catch(done);
   });
 });
