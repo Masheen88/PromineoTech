2f497abf454b8c12885b96c5064bef825fbdf1d0
Masheen1988 <matthew.cox@sumeffect.com>
2022-03-21 14:59:23 -0500

    bootstrap jquery functional

---
+++ Week_10/main.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("submitButton");
+let buttonsByTag = document.getElementsByTagName("button");
+let buttonsByClass = document.getElementsByClassName("form-group");
+console.log(button);
+// console.log(buttonsByTag);
+console.log(buttonsByClass);
