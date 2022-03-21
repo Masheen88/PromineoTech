8f97c8ca05e85da4b0ab71bbc024830a615c7476
Masheen1988 <matthew.cox@sumeffect.com>
2022-03-21 16:56:49 -0500

    Updated Layout

--- Week_10/main.js
+++ Week_10/main.js
@@ -1,6 +1,8 @@
 let button = document.getElementById("submitButton");
 let buttonsByTag = document.getElementsByTagName("button");
 let buttonsByClass = document.getElementsByClassName("form-group");
+let buttonsByCSSClass = document.querySelectorAll("button.btn");
 console.log(button);
 // console.log(buttonsByTag);
-console.log(buttonsByClass);
+// console.log(buttonsByClass);
+console.log(buttonsByCSSClass);
