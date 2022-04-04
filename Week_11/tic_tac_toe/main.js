4efa010e70f1d3c0c187c4e39364c8d250ece3f1
Masheen1988 <matthew.cox@sumeffect.com>
2022-04-04 00:32:29 -0500

    updated gameboard logic

--- Week_11/tic_tac_toe/main.js
+++ Week_11/tic_tac_toe/main.js
@@ -21,7 +21,7 @@ let randomPlayersTurn = randomTurn(players); //Declares the randomly selected pl
 //Displays the current players turn in html
 let div = $(".playerTurn");
 div.append(
-  `<p>Player <span class="gt p${randomPlayersTurn}">${randomPlayersTurn}</span> goes first!</>`
+  `<p class="playerTurnContainer">Player <span class="playerTurnID p${randomPlayersTurn}">${randomPlayersTurn}</span> goes first!</>`
 );
 
 //Condition to call a function based on which player is going first.
@@ -33,11 +33,9 @@ if (randomPlayersTurn <= 1) {
 
 //function to flip the game tiles, iterate turns, and win/lose/draw conditions.
 function flipTiles(firstTurn, secondTurn, firstTurnID, secondTurnID, playerId) {
-  //Declares a blank array all tile data is passed in.
-  let cellsClicked = [];
+  let cellsClicked = []; //Declares a blank array all tile data is passed in.
 
-  //Declares a blank array that will be sorted later in the code.
-  let cellsClickedSorted = [];
+  let cellsClickedSorted = []; //Declares a blank array that will be sorted later in the code.
 
   //A loop to iterate based on the player click on a game tile.
   for (let i = 0; i <= 9; i++) {
@@ -76,32 +74,18 @@ function flipTiles(firstTurn, secondTurn, firstTurnID, secondTurnID, playerId) {
         cellsClicked.shift();
       }
 
-      //Removes any instance of undefined from array.
+      //Removes any instance of undefined from the cellsClickedSorted array.
       let results = cellsClickedSorted.filter(function (index) {
         return index !== undefined;
       });
       console.log("results", results);
-      // //filters the results and outputs an array with no duplicates.
-      // uniqueResults = [...new Set(results)];
 
+      //A condition to check if the player won or the game was a draw.
+
+      //Check if players wins by row 1, column 1 or diagonal spaces (1,5,9)
       if (
         (results[0] == 1 && results[6] == 2 && results[12] == 3) ||
         (results[0] == 3 && results[6] == 2 && results[12] == 1)
-      ) {
-        winGame(playerId);
-      } else if (
-        (results[0] == 4 && results[6] == 5 && results[12] == 6) ||
-        (results[0] == 6 && results[6] == 5 && results[12] == 4)
-      ) {
-        winGame(playerId);
-      } else if (
-        (results[0] == 7 && results[6] == 8 && results[12] == 9) ||
-        (results[0] == 9 && results[6] == 8 && results[12] == 7)
-      ) {
-        winGame(playerId);
-      } else if (
-        (results[0] == 3 && results[6] == 6 && results[12] == 9) ||
-        (results[0] == 9 && results[6] == 6 && results[12] == 3)
       ) {
         winGame(playerId);
       } else if (
@@ -110,13 +94,32 @@ function flipTiles(firstTurn, secondTurn, firstTurnID, secondTurnID, playerId) {
       ) {
         winGame(playerId);
       } else if (
+        (results[0] == 1 && results[6] == 5 && results[12] == 9) ||
+        (results[0] == 9 && results[6] == 5 && results[12] == 1)
+      ) {
+        winGame(playerId);
+      }
+      //Check if players wins by row 3 or column 3
+      else if (
+        (results[0] == 3 && results[6] == 6 && results[12] == 9) ||
+        (results[0] == 9 && results[6] == 6 && results[12] == 3)
+      ) {
+        winGame(playerId);
+      } else if (
+        (results[0] == 7 && results[6] == 8 && results[12] == 9) ||
+        (results[0] == 9 && results[6] == 8 && results[12] == 7)
+      ) {
+        winGame(playerId);
+      }
+      //Check if players wins by row 2, column 2, or diagonal spaces (3,5,7)
+      else if (
         (results[0] == 2 && results[6] == 5 && results[12] == 8) ||
         (results[0] == 8 && results[6] == 5 && results[12] == 2)
       ) {
         winGame(playerId);
       } else if (
-        (results[0] == 1 && results[6] == 5 && results[12] == 9) ||
-        (results[0] == 9 && results[6] == 5 && results[12] == 1)
+        (results[0] == 4 && results[6] == 5 && results[12] == 6) ||
+        (results[0] == 6 && results[6] == 5 && results[12] == 4)
       ) {
         winGame(playerId);
       } else if (
@@ -124,33 +127,27 @@ function flipTiles(firstTurn, secondTurn, firstTurnID, secondTurnID, playerId) {
         (results[0] == 7 && results[6] == 5 && results[12] == 3)
       ) {
         winGame(playerId);
-      } else if (
-        (results[0] == 3 && results[6] == 5 && results[12] == 7) ||
-        (results[0] == 7 && results[6] == 5 && results[12] == 3)
-      ) {
-        winGame(playerId);
-      } else {
-        console.log("Game is a Draw!");
-      }
-
-      if (cellsClicked.length >= 18) {
-        console.log("gameEnd");
+      } else if (results.length == 27) {
+        drawGame();
       }
     });
   }
 
   function winGame(playerId) {
     console.log(`Player ${playerId} Wins!`);
-    let gameTable = $("#gameOver");
+    let gameTable = $(".card-body");
 
-    gameTable;
-
-    return gameTable;
+    gameTable.prepend(
+      `<p> <span class = "${playerId}">${playerId}</span>'s Win!</p>`
+    );
+    $("div.playerTurn").addClass("playerTurnContainer");
+    div.remove();
   }
 
-  function drawGame() {}
+  function drawGame() {
+    console.log("Game is a Draw!");
+    let gameTable = $(".card-body");
+    gameTable.prepend(`<p>The Game is a Draw!</p>`);
+    div.remove();
+  }
 }
-
-//if no tiles match - game ends (draw).
-
-//need logic to display the final results ie win / lose or draw.
