b2fc7ee9830debf58bdeb7972ad9b8369222789b
Masheen1988 <matthew.cox@sumeffect.com>
2022-04-05 10:37:59 -0500

    added additional win conditions

--- Week_11/tic_tac_toe/main.js
+++ Week_11/tic_tac_toe/main.js
@@ -84,7 +84,7 @@ function flipTiles(firstTurn, secondTurn, firstTurnID, secondTurnID, playerId) {
 
       //A condition to check if the player won or the game was a draw.
 
-      //Check if players wins by row 1, column 1 or diagonal spaces (1,5,9)
+      //Win Conditions for 3 turns
       if (
         (results[0] == 1 && results[6] == 2 && results[12] == 3) ||
         (results[0] == 3 && results[6] == 2 && results[12] == 1)
@@ -100,9 +100,7 @@ function flipTiles(firstTurn, secondTurn, firstTurnID, secondTurnID, playerId) {
         (results[0] == 9 && results[6] == 5 && results[12] == 1)
       ) {
         winGame(playerId);
-      }
-      //Check if players wins by row 3 or column 3
-      else if (
+      } else if (
         (results[0] == 3 && results[6] == 6 && results[12] == 9) ||
         (results[0] == 9 && results[6] == 6 && results[12] == 3)
       ) {
@@ -112,9 +110,7 @@ function flipTiles(firstTurn, secondTurn, firstTurnID, secondTurnID, playerId) {
         (results[0] == 9 && results[6] == 8 && results[12] == 7)
       ) {
         winGame(playerId);
-      }
-      //Check if players wins by row 2, column 2, or diagonal spaces (3,5,7)
-      else if (
+      } else if (
         (results[0] == 2 && results[6] == 5 && results[12] == 8) ||
         (results[0] == 8 && results[6] == 5 && results[12] == 2)
       ) {
@@ -129,7 +125,41 @@ function flipTiles(firstTurn, secondTurn, firstTurnID, secondTurnID, playerId) {
         (results[0] == 7 && results[6] == 5 && results[12] == 3)
       ) {
         winGame(playerId);
+      }
+      //Win conditions for 6 turns
+      else if (
+        (results[0] == 1 &&
+          results[3] == 5 &&
+          results[6] == 4 &&
+          results[9] == 7 &&
+          results[12] == 2 &&
+          results[15] == 3) ||
+        (results[0] == 3 &&
+          results[3] == 2 &&
+          results[6] == 7 &&
+          results[9] == 4 &&
+          results[12] == 5 &&
+          results[15] == 1)
+      ) {
+        winGame(playerId);
       } else if (
+        (results[0] == 8 &&
+          results[3] == 5 &&
+          results[6] == 6 &&
+          results[9] == 3 &&
+          results[12] == 9 &&
+          results[15] == 7) ||
+        (results[0] == 7 &&
+          results[3] == 9 &&
+          results[6] == 3 &&
+          results[9] == 6 &&
+          results[12] == 5 &&
+          results[15] == 8)
+      ) {
+        winGame(playerId);
+      }
+      //Win conditions for 7 turns.
+      else if (
         (results[0] == 1 &&
           results[3] == 5 &&
           results[6] == 7 &&
@@ -197,6 +227,131 @@ function flipTiles(firstTurn, secondTurn, firstTurnID, secondTurnID, playerId) {
           results[18] == 1)
       ) {
         winGame(playerId);
+      } else if (
+        (results[0] == 2 &&
+          results[3] == 3 &&
+          results[6] == 7 &&
+          results[9] == 5 &&
+          results[12] == 9 &&
+          results[15] == 6 &&
+          results[18] == 8) ||
+        (results[0] == 8 &&
+          results[3] == 6 &&
+          results[6] == 9 &&
+          results[9] == 5 &&
+          results[12] == 7 &&
+          results[15] == 3 &&
+          results[18] == 2)
+      ) {
+        winGame(playerId);
+      }
+      //Win conditions for 8 turns
+      else if (
+        (results[0] == 1 &&
+          results[3] == 4 &&
+          results[6] == 7 &&
+          results[9] == 6 &&
+          results[12] == 3 &&
+          results[15] == 2 &&
+          results[18] == 9 &&
+          results[21] == 8) ||
+        (results[0] == 8 &&
+          results[3] == 9 &&
+          results[6] == 2 &&
+          results[9] == 3 &&
+          results[12] == 6 &&
+          results[15] == 7 &&
+          results[18] == 4 &&
+          results[21] == 1)
+      ) {
+        winGame(playerId);
+      }
+      //Win conditions for 9 turns
+      else if (
+        (results[0] == 1 &&
+          results[3] == 4 &&
+          results[6] == 7 &&
+          results[9] == 6 &&
+          results[12] == 3 &&
+          results[15] == 8 &&
+          results[18] == 9 &&
+          results[21] == 2 &&
+          results[24] == 5) ||
+        (results[0] == 5 &&
+          results[3] == 2 &&
+          results[6] == 9 &&
+          results[9] == 8 &&
+          results[12] == 3 &&
+          results[15] == 6 &&
+          results[18] == 7 &&
+          results[21] == 4 &&
+          results[24] == 1)
+      ) {
+        winGame(playerId);
+      } else if (
+        (results[0] == 1 &&
+          results[3] == 4 &&
+          results[6] == 7 &&
+          results[9] == 2 &&
+          results[12] == 3 &&
+          results[15] == 8 &&
+          results[18] == 9 &&
+          results[21] == 6 &&
+          results[24] == 5) ||
+        (results[0] == 5 &&
+          results[3] == 6 &&
+          results[6] == 9 &&
+          results[9] == 8 &&
+          results[12] == 3 &&
+          results[15] == 2 &&
+          results[18] == 7 &&
+          results[21] == 4 &&
+          results[24] == 1)
+      ) {
+        winGame(playerId);
+      } else if (
+        (results[0] == 1 &&
+          results[3] == 4 &&
+          results[6] == 7 &&
+          results[9] == 2 &&
+          results[12] == 3 &&
+          results[15] == 6 &&
+          results[18] == 9 &&
+          results[21] == 8 &&
+          results[24] == 5) ||
+        (results[0] == 5 &&
+          results[3] == 8 &&
+          results[6] == 9 &&
+          results[9] == 6 &&
+          results[12] == 3 &&
+          results[15] == 2 &&
+          results[18] == 7 &&
+          results[21] == 4 &&
+          results[24] == 1)
+      ) {
+        winGame(playerId);
+      } else if (
+        (results[0] == 3 &&
+          results[3] == 6 &&
+          results[6] == 9 &&
+          results[9] == 8 &&
+          results[12] == 7 &&
+          results[15] == 4 &&
+          results[18] == 1 &&
+          results[21] == 2 &&
+          results[24] == 5) ||
+        (results[0] == 5 &&
+          results[3] == 2 &&
+          results[6] == 1 &&
+          results[9] == 4 &&
+          results[12] == 7 &&
+          results[15] == 8 &&
+          results[18] == 9 &&
+          results[21] == 6 &&
+          results[24] == 3)
+      ) {
+        winGame(playerId);
+        //If no cobinations match game is a draw.
       } else if (results.length == 27) {
         drawGame();
       }
